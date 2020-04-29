4d487367760744d8facab394369e5e668d26d84a
William Kwao <williamkwao@gmail.com>
2020-04-29 00:39:50 -0400

    update seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -55,7 +55,7 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
@@ -71,10 +71,14 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           property: `og:image`,
-          content: ogImage
-        }
+          content: ogImage,
+        },
+        {
+          name: `twitter:image`,
+          content: ogImage,
+        },
       ].concat(meta)}
-    />
+    ></Helmet>
   )
 }
 
