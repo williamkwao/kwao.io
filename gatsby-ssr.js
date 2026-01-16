d931f78e90c984e50b42ca3afc7036db09d28642
williamkwao <williamkwao@gmail.com>
2026-01-15 20:59:42 -0800

    ssr fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,37 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require('react')
+
+// Fix for Node 18 compatibility with Gatsby 2 SSR
+// Ensure charset meta tag is present to prevent querySelector errors
+exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  // Set HTML attributes including lang
+  setHtmlAttributes({ lang: 'en' })
+  
+  // Ensure charset meta tag is in head components
+  setHeadComponents([
+    React.createElement('meta', { key: 'charset', charSet: 'utf-8' }),
+  ])
+}
+
+// Also ensure charset is present during pre-render
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  
+  // Ensure charset meta tag exists
+  const hasCharset = headComponents.some(
+    component => 
+      component && 
+      component.type === 'meta' && 
+      component.props && 
+      component.props.charSet
+  )
+  
+  if (!hasCharset) {
+    headComponents.unshift(
+      React.createElement('meta', { key: 'charset', charSet: 'utf-8' })
+    )
+    replaceHeadComponents(headComponents)
+  }
+}
